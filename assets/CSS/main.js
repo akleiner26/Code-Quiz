aca09332240dc09d4cad34c1109134c63340672a
orangeblack13 <AndrewJKleiner@gmail.com>
2020-06-07 19:01:16 -0400

    js file added with pseudo code for counter

---
+++ assets/CSS/main.js
@@ -0,0 +1,9 @@
+//update the countdown every one second
+
+//get todays date and time
+
+//find the distance between now and the beginning of the countdown
+
+//time calculations for seconds
+
+//display the result in the element with the id="counter"
