571a885cd27db422c71bb6bd9fa8a847b61cf6cd
orangeblack13 <AndrewJKleiner@gmail.com>
2020-06-11 16:29:52 -0400

    variable issues fixed

--- highScores.js
+++ highScores.js
@@ -1,3 +1,27 @@
+var header = document.querySelector("#header");
+var secondary = document.querySelector("#secondary");
+var responses = document.querySelector("#responses");
+var buttons = document.querySelector("button")
+var startQuiz = document.querySelector("#starter");
+var timeLeft = document.querySelector("#counter");
+var firstButton = document.getElementById("buttonOne");
+var secondButton = document.getElementById("buttonTwo");
+var thirdButton = document.getElementById("buttonThree");
+var fourthButton = document.getElementById("buttonFour");
+var scoreHead = document.getElementById("scoreHead");
+var firstPlaceName = document.getElementById("firstPlaceName");
+var secondPlaceName = document.getElementById("secondPlaceName");
+var thirPlaceName = document.getElementById("thirdPlaceName");
+var firstPlaceScore = document.getElementById("firstPlaceScore");
+var secondPlaceScore = document.getElementById("secondPlaceScore");
+var thirdPlaceScore = document.getElementById("thirdPlaceScore");
+var leaderboardButton = document.getElementById("leaderboard");
+var retakeButton = document.getElementById("retake");
+var scores = [scores];
+var userNames = [userNames];
+
+
+
 var savedUser = localStorage.getItem("userNames");
 var savedScore = localStorage.getItem("scores");
 userNames = JSON.parse(savedUser);
